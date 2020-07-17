83b911b2d3cb3d426441b312496c0c49d6c8e299
huqingfeng <1397649957@qq.com>
2020-07-17 14:21:56 +0800

    0.0.3

--- src/main/java/io/github/jzdayz/controller/MainController.java
+++ src/main/java/io/github/jzdayz/controller/MainController.java
@@ -9,11 +9,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.InputStream;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
@@ -44,6 +46,22 @@ public class MainController {
         headers.add(HttpHeaders.CONTENT_DISPOSITION, String.format("attachment; filename=%s",resource.getFilename()));
 
         return new ResponseEntity<>(resource,headers, HttpStatus.OK);
+    }
+
+    @RequestMapping("show")
+    public ResponseEntity<byte[]> show(String path){
+        HttpHeaders headers = new HttpHeaders();
+        Resource resource = fsService.resource(path.replace("file://",""));
+        headers.add(HttpHeaders.CONTENT_TYPE,"text/html;charset=UTF-8");
+        byte[] data;
+        try (
+                InputStream inputStream = resource.getInputStream();
+                ){
+            data = StreamUtils.copyToByteArray(inputStream);
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        return new ResponseEntity<>(data,headers, HttpStatus.OK);
     }
 
     @ResponseBody
